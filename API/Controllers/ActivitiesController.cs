6a3a428e41e7e6aaa584b29f40970412744a25d7
lexi <byueyue022@gmail.com>
2022-03-03 15:45:34 -0600

    Added delete handler for activities api controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,14 +22,12 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});
         }
 
         [HttpPost]
-
         public async Task<IActionResult> CreateActivity(Activity activity){
             return Ok(await Mediator.Send(new Create.Command{Activity= activity}));
         }
@@ -40,5 +38,12 @@ namespace API.Controllers
             activity.Id =id;
             return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        } 
+        
     }
 }
