151b48a66f2529baef47b2ea4c6076fc0cb1e4a2
lexi <byueyue022@gmail.com>
2022-03-03 15:29:17 -0600

    changed Edit class to update activities using automap

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -19,19 +20,21 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context,IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                activity.Title = request.Activity.Title ?? activity.Title;
+                //activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
                 await _context.SaveChangesAsync();
                 return Unit.Value;
-
             }
         }
     }
