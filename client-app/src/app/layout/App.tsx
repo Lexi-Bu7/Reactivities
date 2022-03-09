11427eb4ce9b979f683581eab338689948d4b99c
lexi <byueyue022@gmail.com>
2022-03-09 12:08:39 -0600

    Add Activity interface
    easy way just google json to ts.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import React,{useEffect, useState} from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/Activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(()=>{
     axios.get('http://localhost:5000/api/Activities').then(response=>{
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivites'/>
         <List>
-        {activities.map((activity: any) =>(
+        {activities.map((activity) =>(
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
